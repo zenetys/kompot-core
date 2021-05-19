e33bb68620a5fb626f154bd2bb5ac14aeea49484
Benoit DOLEZ <bdolez@ant-computing.com>
2021-05-19 23:21:00 +0200

    WIP

--- scripts/sample-inventory.sh
+++ scripts/sample-inventory.sh
@@ -10,8 +10,8 @@ host centos --template
 service PING
 service SNMP
 
-host TEST1:centos 127.0.0.1 parent=SELF
+host TEST1:centos --address=127.0.0.1 parent=SELF
 
-host TEST1 127.0.0.1 parent=SELF
+host TEST1 --address=127.0.0.1 parent=SELF
 
 
